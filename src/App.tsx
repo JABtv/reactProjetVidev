12f85db81dfdc8f075811d73236c9ee094827560
Andres Tom <tom.andres31000@gmail.com>
2024-11-07 16:58:51 +0100

    debut du tuto

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  //state (état, données)
+  const [fruits, setFruits] = useState<Array<{ id: number; nom: string }>>([
+    { id: 1, nom: "Pomme " },
+    { id: 2, nom: "Bannane " },
+    { id: 3, nom: "Poire " },
+    { id: 4, nom: "Abricot " },
+    { id: 5, nom: "Ananas " },
+  ]);
+
+  //comportements
+  const dropElements = (id: number) => {
+    console.log(id);
+  };
+
+  //1. copie du state
+
+  const fruitsCopie = [...fruits];
+
+  //2. manipuler mon state
+
+  const fruitsUpdate = fruitsCopie.filter;
+
+  //Affichage (render)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Liste de fruits</h1>
+      <ul>
+        {fruits.map((fruits) => {
+          return (
+            <li key={fruits.id}>
+              {fruits.nom}
+              <button onClick={() => dropElements(fruits.id)}>X</button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
