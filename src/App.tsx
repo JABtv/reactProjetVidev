316a80c0948f5558c53912ce848b179e18911400
JABtv <tom.andres31000@gmail.com>
2024-11-08 14:51:10 +0100

    Ajout d'un input + button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 function App() {
   //state (état, données)
@@ -10,10 +10,15 @@ function App() {
     { id: 5, nom: "Ananas " },
   ]);
 
+  const [newFruit, setNewFruit] = useState("");
+
+  const changeFruit = (event: ChangeEvent<HTMLInputElement>) => {
+    setNewFruit(event.target.value);
+  };
+
+
   //comportements
   const dropElements = (id: number) => {
-    console.log(id);
-  };
 
   //1. copie du state
 
@@ -21,7 +26,31 @@ function App() {
 
   //2. manipuler mon state
 
-  const fruitsUpdate = fruitsCopie.filter;
+  const fruitsUpdate = fruitsCopie.filter(fruits => fruits.id !== id);
+
+  //3. modifier mon state avec le setter
+
+  setFruits(fruitsUpdate);
+
+  };
+
+  const popElement = (event: ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    //1. copie de state
+
+    const  fruitsCopie = [...fruits]
+
+    //2. manipulation sur la copie du state
+
+    const id = new Date().getTime();
+    const nom = newFruit;
+    fruitsCopie.push({id, nom});
+
+    //3. modiffier le state avec le setter
+
+    setFruits(fruitsCopie);
+    setNewFruit("")
+  };
 
   //Affichage (render)
 
@@ -38,8 +67,19 @@ function App() {
           );
         })}
       </ul>
+      <form action="/" onSubmit={popElement}>
+        <input value={newFruit} type="text" placeholder="Ajouter un fruit..." onChange={changeFruit}/>
+        <button type="submit">Ajouter</button>
+      </form>
     </div>
   );
 }
 
 export default App;
+
+// Gestion du formulaire
+//1. création du formulaire
+//2. soumission du formulaire
+//3. collecte des donnéesss du formulaire
+//3a. méthode 1. symc descendante / ascendante (onChange / handleChange)
+//3b. méthode 2: documentGetElementById "React" (useRef) /!\
